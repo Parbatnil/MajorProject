88398f425dc9489eb2b8003274879ef0ba326712
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-01-26 14:10:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min h-screen flex justify-center items-center bg-gray-900 text-amber-200 text-xl">
+      Major project is under-development
+    </div>
+  );
+};
+
+export default App;
