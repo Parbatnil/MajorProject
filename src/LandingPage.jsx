f019208ab13501a269286484e5b56e09267801e5
ARNABDHUA <arnabdhua74@gmail.com>
2025-01-29 01:15:57 +0530

    feedback

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import CoursesAndExam from "./components/CoursesAndExam";
 import TrendCourse from "./components/TrendCourse";
+import Feedback from "./components/Feedback";
 
 const LandingPage = () => {
   return (
     <div>
       <CoursesAndExam />
       <TrendCourse />
+      <Feedback/>
     </div>
   );
 };
